f8926888f7e424b69eefb2c131d0e440a3af3aea
ridwankechil <70096802+ridwankechil@users.noreply.github.com>
2020-11-26 14:46:46 +0700

    Create Hc.sh

---
+++ Hc.sh
@@ -0,0 +1,165 @@
+#!/system/xbin/bash
+#mau Recode izin dulu di Instagram Ridwan58aja
+#Github:https://github.com/RidwanKechil
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+steep 0,01
+echo
+echo
+clear
+
+cd /sdcard
+sleep 1
+echo "  "
+echo
+echo
+clear
+
+sleep 1
+echo "\033[32;1m selamat datang siapa nama kamu?" #tulisankeluar
+read nick #membaca tulisan
+echo
+echo
+clear
+
+sleep 3
+echo "\033[32;1m
+#################################
+#-AUTHOR= RIDWAN-KECHIL.. #
+#-subs. = Ridwan Kechil. #
+#-Team=THEIMMORTALHACKER #
+#################################"
+sleep 1
+echo "SELAMAT BERGABUNG" $nick
+echo "---------------------------------------------"
+sleep 2
+pkg update
+clear
+pkg upgrade
+clear
+pkg install python
+clear
+pip install requests 
+clear
+pip install mechanize
+clear
+pip inatall bs4
+clear
+
+sleep 1
+echo "Terimakasih" $nick
+sleep 2
+echo "{<root:10.7:><:masukkan disini}}>>" #tulisankeluar
+read nick1; #membaca tulisan
+sleep 2
+echo
+echo
+clear
+
+sleep 3
+echo $ku "
+#################################
+#-AUTHOR= MR. WRpy
+#class = Next
+#info =new update 26-11-20
+################################# "
+sleep 1
+echo $me "masukkan nomor hp anda " 
+echo "di butuhkan pulsa 35k"
+echo "100%Aman " #tulisankeluar
+read nomor;  #membaca tulisan
+sleep 3
+echo
+echo
+clear
+
+sleep 1
+echo $ku "##20%"
+sleep 1
+echo $pu "###30%"
+sleep 1
+echo $bi "####40%"
+sleep 2
+echo $or "#####50%"
+sleep 3
+echo $me "######60%"
+sleep 6
+echo $cy "#######70%"
+sleep 8
+echo $bi "########80%"
+sleep 10
+echo $ku "#########90%"
+sleep 16
+echo $ij "##########100%"
+echo "------------>>>>>>"
+echo
+echo
+clear
+
+sleep 3
+echo $ku "
+#################################
+#-AUTHOR= RIDWAN-KECHIL.. #
+#-subs. = Ridwan Kechil. #
+#-Team=THEIMMORTALHACKER #
+################################# "
+sleep 1
+echo "sabar tunggu beberapa menit"
+sleep 4
+echo "_____________________________________"
+sleep 2
+echo "Pencarian berkas"
+sleep 2
+echo "#10%"
+sleep 1
+echo "#######""100%"
+sleep 3
+echo "estrak"
+sleep 2
+echo "estrak file Anda"
+sleep 1
+echo "tunggu sampai selesai"
+sleep 4
+echo $bi "_0%"
+sleep 1
+echo "##_20%"
+sleep 2
+echo "####_40%"
+sleep 3
+echo "######_60%"
+sleep 4
+echo "########_80%"
+sleep 8
+echo "##########_100%"
+sleep 5
+echo $ku "____________________________________"
+sleep 2
+echo "selesai "
+sleep 1
+echo $me $nomor "{Pulsa Berhasil Di Ambil}"
+sleep 1
+echo "kata sandi anda "
+sleep 6
+echo $cy "Gmail=" $nick1
+echo $ij #allPasword #pw= $ku "===>>>    sandi =>> GueDimas " #AllPassword
+sleep 1
+echo $me "kata sandi aktif "
+echo "___________________________________ "
+sleep 1
+echo "file di hapus silahkan coba semoga work "
+echo "dan jangan lupa untuk kembali "
+echo "sandi akan hilang dalam waktu 120 detik "
+sleep 120
+echo
+echo
+clear
+
+sleep 1
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "jangan lupa kembali & semoga berhasil"
