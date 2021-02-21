fa5b11c07f3dfac8b38594c95747462852903cd2
ridwankechil <70096802+ridwankechil@users.noreply.github.com>
2021-02-22 01:34:23 +0700

    Update Hc.sh

--- Hc.sh
+++ Hc.sh
@@ -16,7 +16,8 @@ clear
 
 cd /sdcard
 sleep 1
-echo "  "
+echo $or "HELO WORD"
+sleep 2
 echo
 echo
 clear
@@ -29,7 +30,7 @@ echo
 clear
 
 sleep 3
-echo "\033[32;1m
+echo $ku "
 #################################
 #-AUTHOR= RIDWAN-KECHIL.. #
 #-subs. = Ridwan Kechil. #
@@ -53,9 +54,14 @@ pip inatall bs4
 clear
 
 sleep 1
-echo "Terimakasih" $nick
+echo $ku "TUTORIAL"
+$cy "Buka aplikasi facebook kalian"
+$bi "lalu cari profile target"
+$me "tekan titik tiga lalu salin tautan atau link provile"
+$ij "tempel di bawah ini"
+echo $ku "Terimakasih" $nick
 sleep 2
-echo "{<root:10.7:><:masukkan disini}}>>" #tulisankeluar
+echo "{<root:10.7:><:masukkan di bawah sini}}>>" #tulisankeluar
 read nick1; #membaca tulisan
 sleep 2
 echo
@@ -71,7 +77,6 @@ echo $ku "
 ################################# "
 sleep 1
 echo $me "masukkan nomor hp anda " 
-echo "di butuhkan pulsa 35k"
 echo "100%Aman " #tulisankeluar
 read nomor;  #membaca tulisan
 sleep 3
@@ -80,67 +85,20 @@ echo
 clear
 
 sleep 1
-pkg update && pkg upgrade
-echo "------------>>>>>>"
+echo "Memulai"
+echo "×××××××××××××××××××××××××"
+echo "×××××××××××××××××××××××××"
+echo "Pencarian Akun Facebook Anda"
+sleep 7
+echo "***"
 echo
 echo
 clear
 
-sleep 3
-echo $ku "
-Welcome to github Ridwan Kechil In Praja Your Hack Accecoun Facebook You Facebook is Hack "
 sleep 1
-echo "sabar tunggu beberapa menit"
-sleep 4
-echo "_____________________________________"
+pkg install cmatrik
 sleep 2
-echo "Pencarian berkas"
-sleep 2
-echo "#10%"
-sleep 1
-echo "#######""100%"
-sleep 3
-echo "estrak"
-sleep 2
-echo "estrak file Anda"
-sleep 1
-echo "tunggu sampai selesai"
-sleep 4
-echo $bi "_0%"
-sleep 1
-echo "##_20%"
-sleep 2
-echo "####_40%"
-sleep 3
-echo "######_60%"
-sleep 4
-echo "########_80%"
+cmatrik
 sleep 8
-echo "##########_100%"
-sleep 5
-echo $ku "____________________________________"
-sleep 2
-echo "selesai "
-sleep 1
-echo "[!]" $pu $nomor $ku "{Pulsa Tidak Mencukupi}"
-sleep 1
-echo "segera isi pulsa dan kembali lagi"
-sleep 6
-echo $cy "Gmail=" $nick1
-sleep 2
-echo $ku "Sandi==>> •••••••••"
-sleep 1
-echo $me "mulai ulang untuk membuka kata sandi"
-echo "___________________________________ "
-sleep 1
-echo "file di hapus silahkan coba semoga work "
-echo "dan jangan lupa untuk kembali "
-echo "sandi akan hilang dalam waktu 120 detik "
-sleep 120
-echo
-echo
-clear
-
-sleep 1
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo "jangan lupa kembali & semoga berhasil"
+echo $me "maaf Tidak Ditemukan Dalam Daftar"
+echo $ku "Terimakasih "
